45759503a64a6600425bba7855572e9e191bb5cd
vladislv31 <vladislv.k31@gmail.com>
2022-02-16 13:12:57 +0200

    crontab task
    remove spaces from crontab.txt

---
+++ scripts/cron_date.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo $(date '+%m/%d/%Y - %H:%M:%S') >> $HOME/cron_dates.txt
---
+++ scripts/cron_weather.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl "https://api.openweathermap.org/data/2.5/weather?lat=49&lon=36&appid=107500919e0c07cdd7d5f968f1307be7" -o $HOME/weather.json
